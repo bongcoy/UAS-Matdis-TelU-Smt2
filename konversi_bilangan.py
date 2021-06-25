be90eb04a65f3ead7b459d00b3242494a79b7583
BgsHryd <76993913+BgsHryd@users.noreply.github.com>
2021-06-25 19:53:21 +0700

    Update konversi_bilangan.py
    nyuruh programnya diet(biar ramping) sama ngebikin dia bisa ngehandle basis yang lebih dari 10 bahkan dengan input berupa huruf

--- konversi_bilangan.py
+++ konversi_bilangan.py
@@ -1,20 +1,17 @@
 '''
 Kekurangan program :
-1. Harus convert bentuk Hexa (yang ada hurufnya) dengan manual, contoh : ketika ada A, maka harus menuliskan 10
-2. Tida bisa merubah KE Hexa berbentuk huruf (khusus untuk sisa berangka 10 - 16),
-jadi yang tertampil adalah angka 10 - 16 bukan A - G
+menunggu repiw dari testing
 
 created by Muhammad Pascal Dewantara, Informatika'20 Telkom University
+edited by Bagus Hariyadi 
+p.s tinggal di testing ae bismillah kaga ada yang salah
 '''
-
-"""COBA Gua ubah ya ini"""
-"""Bentar ini yang ketiga """
 
 def konversi_kali(x,basis_x):
     '''
     konversi x to desi
     '''
-    if basis_x != 16 or x != 0:
+    if basis_x <= 10:
         lis = [int(i) for i in str(x)]
         total = 0
         jmlh_digit = len(lis) - 1
@@ -30,18 +27,13 @@ def konversi_kali(x,basis_x):
             total += hasil
             jmlh_digit -= 1
 
-    elif basis_x == 16 and x == 0:
-        lis_hexa = []
-        selesai = False
-        digit = 1
-        while not selesai:
-            tanya = input(f"Digit ke-{digit} (dari kiri) = ")
-            digit+=1
-            if tanya == "selesai":
-                selesai = True
-                break
-
-            lis_hexa.append(int(tanya))
+    elif basis_x >10:
+        lis_hexa = [ord(i) for i in x]
+        for i in range(len(lis_hexa)):
+            if lis_hexa[i]>=48 and lis_hexa[i]<=57:
+                lis_hexa[i] = lis_hexa[i] - 48
+            elif lis_hexa[i]>=65 and lis_hexa[i]<=90:
+                lis_hexa[i] = lis_hexa[i] - 55
         
         total = 0
         jmlh_digit = len(lis_hexa) - 1
@@ -75,6 +67,9 @@ def konversi_sisa(desimal,basis_y):
         if hasil != 0:
             aksi(hasil,basis_y)
     aksi(desimal,basis_y)
+    for i in range(len(lis)):
+        if lis[i] >9:
+            lis[i] = chr(lis[i]+55)
     print()
 
     for i in lis[::-1]:
@@ -88,16 +83,12 @@ def tanya_input():
         konversi_sisa(desi,bas_y)
     elif mau == "kali":
         basis_bil = int(input("Basis asal (tuliskan dlm bntk angka) = "))
-        if basis_bil != 16:
+        if basis_bil < 10:
             bil = int(input("Bilangan (tuliskan dlm bntk angka) = "))
             konversi_kali(bil,basis_bil)
-        elif basis_bil == 16: #Parameter bilangan jadi ga wajib, karena kita input 1 per 1 untuk hexa
-            ada_huruf = input("Ada huruf (iye atau ga) ? ")
-            if ada_huruf == "iye":
-                print('Ketik "selesai" apabila sudah tertulis semua\n')
-                konversi_kali(0,basis_bil)
-            if ada_huruf == "ga":
-                bil = int(input("Bilangan (tuliskan dlm bntk angka, tanpa spasi) = "))
-                konversi_kali(bil,basis_bil)          
+        elif basis_bil > 10: #Parameter bilangan jadi ga wajib, karena kita input 1 per 1 untuk hexa
+            bil = input("masukkin aja apa yang mau dikonversinya: ")
+            bil = bil.upper()
+            konversi_kali(bil,basis_bil)    
 
 tanya_input()
