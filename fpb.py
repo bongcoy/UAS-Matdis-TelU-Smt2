0660981d54344405176aeb1c15485b151cc160fa
BgsHryd <76993913+BgsHryd@users.noreply.github.com>
2021-06-26 18:55:13 +0700

    Create fpb.py
    ini buat nyari fpb gan

---
+++ fpb.py
@@ -0,0 +1,11 @@
+def fpb():
+    arr = [int(i) for i in input().split()]
+    arr.sort()
+    for i in range(1,arr[0]+1):
+        stat = 0
+        for j in range(len(arr)):
+            if arr[j] % i == 0:
+                stat+=1
+        if stat == len(arr):
+            fpb = i
+    print(f"FPB atau GCD nya adalah: {fpb}")
