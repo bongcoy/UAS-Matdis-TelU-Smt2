43066e05a592c01fa6ebd9d1e01dff72384d2e45
BgsHryd <76993913+BgsHryd@users.noreply.github.com>
2021-06-28 08:29:53 +0700

    Create faktorisasiprima.py

---
+++ faktorisasiprima.py
@@ -0,0 +1,14 @@
+# fungsi untuk mencari faktorisasi prima sebuah bilangan
+'''credit to https://adityarizki.net/belajar-python-faktorisasi-prima-sebuah-bilangan/'''
+def faktorisasiprima(x):
+    # keluaran variabel factorlist berupa array
+    factorlist=[]
+    loop=2
+    while loop<=x:
+        if x%loop==0:
+            x/=loop
+            # append berfungsi untuk menambahkan sebuah objek ke dalam list/daftar
+            factorlist.append(loop)
+        else:
+            loop+=1
+    return factorlist
