26c261afc2c6c2de307d8d1d9b8a3cd8c2944bc2
Cal <62139304+bongcoy@users.noreply.github.com>
2021-06-28 10:15:03 +0700

    Update bezout.py

--- bezout.py
+++ bezout.py
@@ -15,7 +15,8 @@ def bezout(a, b, x = 0, prev_x = 1, y = 1, prev_y = 0):
 
 	# if remainder is 0, stop here : gcd found
 	if remainder == 0:
-		return b, x, y
+		print("*note : s adalah koefisien bilangan yg lebih kecil")
+		print(f"FPB = {b}, s = {x}, t = {y}") 
 
 	# else, update x and y, and continue
 	quotient = a // b
