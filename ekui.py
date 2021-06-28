c27b88ca333b4a551379ea56584454be70904ed2
Cal <62139304+bongcoy@users.noreply.github.com>
2021-06-28 09:24:39 +0700

    program ax = b (mod m)

---
+++ ekui.py
@@ -0,0 +1,10 @@
+def ekui(a,b,m,max):
+    for x in range(max+1):
+        if ((a*x)-b) % m == 0:
+            print("x =",x)
+def tanya_input():
+    print("[ekui.tanya_input()]")
+    print("Masukkan dengan format :\nax = b (mod m)\n\n*note = jangan lupa operasiin si b nya dulu kalo misal ada konstanta lain\n")
+    a,b,m,max = map(int,input("Urutan 'a b m max'\ninput : ").split())
+    ekui(a,b,m,max)
+tanya_input()
