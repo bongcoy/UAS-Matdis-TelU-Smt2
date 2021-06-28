15d711274bb77e3ae303fe65dff6c73e5b2d1e90
Cal <62139304+bongcoy@users.noreply.github.com>
2021-06-28 10:37:49 +0700

    Update bezout.py

--- bezout.py
+++ bezout.py
@@ -1,6 +1,6 @@
 "Credit to : https://github.com/BaReinhard/Hacktoberfest-Mathematics/blob/master/algebra/bezout/python/bezout.py"
 
-print("Ketik 'bezout.bezout(a,b)'")
+print("Ketik 'bezout.bez(a,b)'")
 def bezout(a, b, x = 0, prev_x = 1, y = 1, prev_y = 0):
 	""" Calculate the Bézout's identity of 'a' and 'b' recursively
 		Using the extended euclidean algorithm
@@ -15,10 +15,12 @@ def bezout(a, b, x = 0, prev_x = 1, y = 1, prev_y = 0):
 
 	# if remainder is 0, stop here : gcd found
 	if remainder == 0:
-		pengen_ngeprint_ini_sad = f"\n*note : x adalah koefisien bilangan yg lebih kecil\nFPB = {b}, x = {x}, y = {y}"
 		return b, x, y
 	
 	# else, update x and y, and continue
 	quotient = a // b
 	prev_x, prev_y, x, y = x, y, quotient*x + prev_x, quotient*y + prev_y
 	return bezout(b, remainder, x, prev_x, y, prev_y)
+def bez(a,b):
+	print(f"*note : x adalah koef bilangan yang terbesar\nFormat Jawaban : (FPB, x, y)")
+	print(bezout(a, b, x, prev_x, y, prev_y))
